423f03cbb0092982c3ca1fa9f88789563baeb7cf
João Vitor Fonseca <116322328+Rickmall1@users.noreply.github.com>
2023-05-30 15:39:18 -0300

    Questão 5 (ainda)

--- Res5_cap5.c
+++ Res5_cap5.c
@@ -2,23 +2,27 @@
 #include <locale.h>
 #include <math.h>
 
-int main()
-{
-  setlocale(LC_ALL, "Portuguese");
+int main() {
+    setlocale(LC_ALL, "Portuguese");
 
-  int fat = 0, x = 0, sinal = 1, S = 0;
-  int i;
+    int fat = 0, numerador, sinal = 1, S = 0;
+    int i;
 
-  printf("Digite x: \n");
-  scanf("%d", &x);
+    printf("Digite o numerador: \n");
+    scanf("%d", &numerador);
 
-  // Potencia par = negativo ; Potencia impar = positivo;
-  for (i = 2; i <= x; i++)
-  {
-    if ((i % 2 == 0))
-    {
-      sinal = sinal * -1;
+    // Sinal baseado na potencia
+    // par = negativo ; impar = positivo;
+    for (i = 2; i <= numerador; i++) {
+        if ((i % 2 == 0)) {
+            numerador = numerador*-1;
+        } else {
+            numerador = numerador*1;
+        }
+        //Definindo a potencia do numerador;
+        numerador = pow(numerador, i);
+        printf("%d\n", numerador);
     }
-  }
-  return 0;
+
+    return 0;
 }
