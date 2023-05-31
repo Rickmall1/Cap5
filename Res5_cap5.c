7d1e9345f5aa6de45d0b4659855e4209d87011f9
João Vitor Fonseca <116322328+Rickmall1@users.noreply.github.com>
2023-05-31 12:26:20 -0600

    Ainda na questão 5

--- Res5_cap5.c
+++ Res5_cap5.c
@@ -5,23 +5,18 @@
 int main() {
     setlocale(LC_ALL, "Portuguese");
 
-    int fat = 0, numerador, sinal = 1, S = 0;
-    int i;
+    int numerador,expoente,denominador,total;
 
     printf("Digite o numerador: \n");
     scanf("%d", &numerador);
 
-    // Sinal baseado na potencia
-    // par = negativo ; impar = positivo;
-    for (i = 2; i <= numerador; i++) {
-        if ((i % 2 == 0)) {
-            numerador = numerador*-1;
-        } else {
-            numerador = numerador*1;
+   
+    for(expoente=2;expoente<=5;expoente++){
+        if((expoente%2==0)){
+            numerador = numerador*(-1);
         }
-        //Definindo a potencia do numerador;
-        numerador = pow(numerador, i);
-        printf("%d\n", numerador);
+        total = pow(numerador,expoente);
+        printf("%d\n", total);
     }
 
     return 0;
